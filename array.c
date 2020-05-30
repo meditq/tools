addaae183aba7f0588d164cc45c5a77f1fab5ccf
meditq <samplepassname@gmail.com>
2020-05-30 21:11:58 +0900

    array実装（未テスト）

--- array.c
+++ array.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int expand(array a);
+int shrink(array a);
+
 array array_new(void){
 	array a = malloc(sizeof(struct _array));
 	if(a == NULL) return NULL;
@@ -11,6 +14,38 @@ array array_new(void){
 	}
 	a->size = 0;
 	a->heapsize = 1;
+	return a;
+}
+
+int expand(array a){
+	if(a->size >= a->heapsize){
+		int *vstmp = realloc(a->vs, sizeof(int) * a->heapsize * 2);
+		if(vstmp == NULL) return 1;
+		a->vs = vstmp;
+		a->heapsize *= 2;
+	}
+	return 0;
+}
+
+int shrink(array a){
+	if(a->size <= a->heapsize / 2 && a->heapsize > 1){
+		int *vstmp = realloc(a->vs, sizeof(int) * a->heapsize / 2);
+		if(vstmp == NULL) return 1;
+		a->vs = vstmp;
+		a->heapsize /= 2;
+	}
+	return 0;
+}
+	
+int push(array a, int val){
+	if(!expand(a)) return 1;
+	a->vs[a->size++] = val;
+	return 0;
+}
+
+int pop(array a){
+	int val = a->vs[--a->size];
+	shrink(a);
 	return a;
 }
 
--- array.h
+++ array.h
@@ -4,4 +4,6 @@ struct _array {
 } * array;
 
 array array_new(void);
+int push(array a, int val);
+int pop(array a);
 void array_free(array a);
